a62f7d258df7abf3d0a33c4905a4b3cc1fd9bd2c
DarkNormal <mark.lordan@me.com>
2022-09-11 22:28:02 +0100

    Spring Security user authentication update gitignore

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -26,7 +26,8 @@ public class TodoService {
 	}
 	
 	public List<Todo> findByUsername(String username){
-		return todos;
+		Predicate<Todo> filterByUsername = todo -> todo.getUsername().equalsIgnoreCase(username);
+		return todos.stream().filter(filterByUsername).toList();
 	}
 	
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
