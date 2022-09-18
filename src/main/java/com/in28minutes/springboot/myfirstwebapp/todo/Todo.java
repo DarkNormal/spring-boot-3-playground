2c21a6f34e225b70db320624e7d2c4b6f9910aa1
DarkNormal <mark.lordan@me.com>
2022-09-18 22:24:53 +0100

    Mark pojo as entity, DB init with sql file

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,11 +2,15 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 //Database (MySQL) 
 //Static List of todos => Database (H2, MySQL)
 
+@Entity
 public class Todo {
 
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -18,6 +22,8 @@ public class Todo {
 		this.done = done;
 	}
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	
